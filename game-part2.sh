9e3b39d7eaad4604333f1aaf5dd4ad93cde681d3
YourBoyRory <yourboyrory@gmail.com>
2024-04-24 10:44:46 -0400

    Fixed Library to work with imports

--- game-part2.sh
+++ game-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # say $message
-function say {
+say() {
     echo "$1"
     (
         #killall xcowsay 2>/dev/null
@@ -10,10 +10,9 @@ function say {
     espeak "$1" 2>/dev/null
     sleep 1
 }
-export -f say
 
 # testHash $input $hash
-function testHash {
+testHash() {
     local hash=$(echo -n "$1" | md5sum | awk '{print $1;}')
     if [[ $hash == $2 ]]; then
         return 1
@@ -21,10 +20,9 @@ function testHash {
         return 0
     fi
 }
-export -f testHash
 
 # promt $hash
-function promt {
+promt() {
     if xset q &>/dev/null; then
         local pass=$(yad --text="$1" --entry --button=Done)
     else
@@ -33,4 +31,18 @@ function promt {
     fi
     testHash "$pass" "$1"
 }
-export -f promt
+
+say "Hello Cybertern."
+say "I'd like to play a game."
+say "A game that will test your wits, and your endurance..."
+say "Your first test will not be easy."
+say "You may want to ask your friend John..."
+
+while promt "83218ac34c1834c26781fe4bde918ee4"; do
+    say "That wasn't quite right."
+done
+
+say "Excelent Work Cybertern"
+say "I would say that..."
+say "If my grandma hadn't solved it 5 times faster."
+
