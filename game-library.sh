84f2f8a6e7e395cd99e04498b6f052a0757dc7f1
YourBoyRory <yourboyrory@gmail.com>
2024-04-24 10:49:00 -0400

    Fixed Library to work with imports

--- game-library.sh
+++ game-library.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # say $message
-say() {
+function say {
     echo "$1"
     (
         #killall xcowsay 2>/dev/null
@@ -10,9 +10,10 @@ say() {
     espeak "$1" 2>/dev/null
     sleep 1
 }
+export -f say
 
 # testHash $input $hash
-testHash() {
+function testHash {
     local hash=$(echo -n "$1" | md5sum | awk '{print $1;}')
     if [[ $hash == $2 ]]; then
         return 1
@@ -20,9 +21,10 @@ testHash() {
         return 0
     fi
 }
+export -f testHash
 
 # promt $hash
-promt() {
+function promt {
     if xset q &>/dev/null; then
         local pass=$(yad --text="$1" --entry --button=Done)
     else
@@ -31,3 +33,4 @@ promt() {
     fi
     testHash "$pass" "$1"
 }
+export -f promt
