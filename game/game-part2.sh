451db4258bd362dd91ad7912de6abd48837e1ef5
YourBoyRory <yourboyrory@gmail.com>
2024-04-24 18:18:07 -0400

    renamed promt to prompt

--- game/game-part2.sh
+++ game/game-part2.sh
@@ -11,8 +11,10 @@ say "Your first test will not be easy."
 sayEnd "You may want to ask your friend John..."
 
 
-while promtPassword "83218ac34c1834c26781fe4bde918ee4"; do
-    sayEnd "That wasn't quite right."
+while prompt "83218ac34c1834c26781fe4bde918ee4"; do
+    say "That wasn't quite right."
+    sayEnd "You may want to ask your friend John..."
+    echo done
 done
 
 say "Excelent Work Cybertern!"
