4aad56b233124b0b9043ca0fe60c0b51788e9693
YourBoyRory <yourboyrory@gmail.com>
2024-04-24 11:13:00 -0400

    Started work on GUI portion of the game

---
+++ game/game-part2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source "../lib/main-library.sh"
+
+say "Hello Cybertern."
+say "I'd like to play a game."
+say "A game that will test your wits, and your endurance..."
+say "Your first test will not be easy."
+say "You may want to ask your friend John..."
+
+while promt "83218ac34c1834c26781fe4bde918ee4"; do
+    say "That wasn't quite right."
+done
+
+say "Excelent Work Cybertern"
+say "I would say that..."
+say "If my grandma hadn't solved it 5 times faster."
+
