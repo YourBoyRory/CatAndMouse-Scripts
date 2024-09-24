8bdc8086a1ab5871c502362337b5ee672d022493
TheDRBatt <130406777+TheDRBatt@users.noreply.github.com>
2024-09-24 19:46:13 -0400

    Added two sections to game-demo.sh

--- game/game-demo.sh
+++ game/game-demo.sh
@@ -16,17 +16,40 @@ while prompt "40be4e59b9a2a2b5dffb918c0e86b3d7"; do
     sayEnd "You may want to ask your friend John..."
 done
 
-say "Excelent Work Cybertern!"
+say "Excellent Work Cybertern!"
 sleep .5
 say "Is what I WOULD say..."
 (sleep 2.8; playSound "../assets/boom.wav")&
 sayEnd "If my grandma hadn't solved it 5 times faster."
 
 say "Maybe I should give you something easier."
-say "Surly you can handle some basic mathematics."
+say "Surely you can handle some basic mathematics."
 sayEnd "What is 32 times 16?"
 
 while prompt "10a7cdd970fe135cf4f7bb55c0e3b59f" "32*16"; do
     sayEnd "That wasn't quite right."
 done
 say "Ah, One of my favorite numbers!"
+say "Next question."
+say "b10304c0616186875440278d4af42ede."
+
+while prompt "98f13708210194c475687be6106a3b84" "b10304c0616186875440278d4af42ede"; do
+    say "You fool!"
+    sayEnd "I'm not saying it again."
+done
+
+say "Good."
+
+say "Alright, you have solved these one-off puzzles."
+say "But now for something that will test your endurance."
+sayEnd "How deep does this rabbit hole go?"
+
+while prompt "ca8a2087e5557e317599344687a57391" "d562bcef9dae0c409238ca1b22affabd"; do
+    say "You must go deeper."
+done
+sayEnd "Perfect."
+
+
+
+testHash "$pass" "$1"
+return testHash "$pass" "$1"
